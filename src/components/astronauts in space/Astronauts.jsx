81c3d5ba5e3fd0a5ff3cbd15db2d3946225731bc
Pratham Matkar <fs20if005@gmail.com>
2025-12-20 17:35:21 +0530

    Version 1

--- src/components/astronauts in space/Astronauts.jsx
+++ src/components/astronauts in space/Astronauts.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import "./Astronauts.css";
 
 const Astronauts = () => {
-  
+
   const [astronauts, setAstronauts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -12,7 +12,7 @@ const Astronauts = () => {
       try {
         const api_key = import.meta.env.VITE_ASTRONAUTS_IN_SPACE_API;
         const res = await fetch(
-          `http://api.scrapestack.com/scrape?access_key=${api_key}&url=https://whoisinspace.com/`
+          // `http://api.scrapestack.com/scrape?access_key=${api_key}&url=https://whoisinspace.com/`
         );
 
         const html = await res.text();
